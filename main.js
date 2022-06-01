197e9746b1acc34fe6f19dae4f76cb4d1f000906
wonmoLee <leewonmo94@gmail.com>
2022-06-01 11:03:59 +0900

    axios instance 작업

--- main.js
+++ main.js
@@ -9,21 +9,17 @@
  */
 
 const electron = require('electron');
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, ipcMain} = electron;
 const url = require('url');
 const path = require('path');
 const io = require('socket.io-client');
 const axios = require('axios');
-const socketURL = 'ws://127.0.0.1:3000';
-const socketOptions = {
-    transports: ['websocket'],
-    forceNew: true,
-    query: {
-        token: "wonmoLee"
-    }
-}
+const httpInstance = axios.create({
+    baseURL: 'http://127.0.0.1:3000'
+});
 const handler_manager = require('./handler_manager');
 const SocketService = require('./service/socketService');
+const { ipcRenderer } = require('electron');
 
 let win;
 let socket;
@@ -44,19 +40,45 @@ app.on('ready', ()=>{
     win = new BrowserWindow(options);
     win.loadURL(url.format({
         pathname: path.join(__dirname, 'login.html'),
-        protocol: 'file'
+        protocol: 'file',
+        slashes: true
     }));
     win.webContents.openDevTools(); // 개발자도구
     win.once('ready-to-show', ()=>{
-        socket = SocketService.createSocket(io, socketURL, socketOptions);
-        SocketService.addHandlers(socket, win, handler_manager);
+        console.log('ready-to-show');
         win.show();
     });
     win.on('closed', ()=>{
+        console.log('window closed');
         win = null;
         app.quit();
     })
 });
+
+ipcMain.on('signInRequest', (event, message)=>{
+    httpInstance.post('/users/login', message)
+    .then((response)=>{
+        const socketURL = 'ws://127.0.0.1:3000';
+        const socketOptions = {
+            transports: ['websocket'],
+            forceNew: true,
+            query: {
+                token: response.data.token
+            }
+        };
+        socket = SocketService.createSocket(io, socketURL, socketOptions);
+        SocketService.addHandlers(socket, win, handler_manager);
+        event.sender.send('signInRequest-Success', response);
+    })
+    .catch((error)=>{
+        const result = {
+            status: error.response.status,
+            statusText: error.response.statusText
+        }
+        event.sender.send('signInRequest-Failed', result);
+    });
+})
+
 app.on('window-all-closed', ()=>{
     app.quit();
 });
--- webScripts/login.js
+++ webScripts/login.js
@@ -15,4 +15,45 @@
     ipcRenderer.on(socketEvent.HELLO, (event, message)=>{
         console.log(message);
     });
+    const userIdInput = document.getElementById('user-id-input');
+    const userPasswordInput = document.getElementById('user-password-input');
+    const signInButton = document.getElementById('button-SignIn');
+    const signUpButton = document.getElementById('button-SignUp');
+
+    signInButton.addEventListener('clcik', ()=>{
+        console.log('click');
+        const id = userIdInput.value;
+        const password = userPasswordInput.value;
+        const parameter = {
+            id: id,
+            password: password
+        };
+        ipcRenderer.send('signInRequest', parameter);
+    });
+    ipcRenderer.on('signInRequest-Success', (event, message)=>{
+        console.log(message);
+        alert(message.statusText);
+    })
+    ipcRenderer.on('signInRequest-Failed', (event, message)=>{
+        console.log(message);
+        alert(message.statusText);
+    })
+    signUpButton.addEventListener('clcik', ()=>{
+        console.log('click');
+        const id = userIdInput.value;
+        const password = userPasswordInput.value;
+        const parameter = {
+            id: id,
+            password: password
+        };
+        ipcRenderer.send('signUpRequest', parameter);
+    });
+    ipcRenderer.on('signUpRequest-Success', (event, message)=>{
+        console.log(message);
+        alert(message.statusText);
+    })
+    ipcRenderer.on('signUpRequest-Failed', (event, message)=>{
+        console.log(message);
+        alert(message.statusText);
+    })
 })();
