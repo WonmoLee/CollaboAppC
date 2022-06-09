484ac3f8d725ed22a4f4c69f26418a172e9cd465
wonmoLee <62128949+wonmoLee@users.noreply.github.com>
2022-06-09 11:20:52 +0900

    window 변경시 새로운 핸들러 등록을 위한 작업

--- service/socketService.js
+++ service/socketService.js
@@ -10,10 +10,20 @@
 
 exports.createSocket = (io, socketURL, socketOptions)=>{
     return io(socketURL, socketOptions);
-}
+};
 
 exports.addHandlers = (socket, win, handlerManager)=>{
+    let listeners = [];
     handlerManager.forEach(handler=>{
-        socket.on(handler.event, handler.handler.bind(null, socket, win));
+        let callback = handler.handler.bind(null, socket, win);
+        listeners.push(callback);
+        socket.on(handler.event, callback);
     });
+    return listeners;
+};
+
+exports.addHandler = (socket, win, handler)=>{
+    let listener = handler.handler.bind(null, socket, win);
+    socket.on(handler.event, listener);
+    return listener;
 }
